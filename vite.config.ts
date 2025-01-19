a30297a6b855648f2fa658b2a63085ab0c0cdaa9
정규영 <jgy_98@naver.com>
2025-01-19 20:35:57 +0900

    chore : 프로젝트 기본 세팅 + styled-components 적용

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      srcDir: 'src',
+      filename: 'sw.ts',
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'React Vite PWA',
+        short_name: 'PWA',
+        description: 'A React + Vite + PWA Application',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
