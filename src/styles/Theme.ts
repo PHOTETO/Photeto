12208a0c2b6e88b102dfa7bd796a1b3e8b550e5e
정규영 <jgy_98@naver.com>
2025-03-22 13:06:08 +0900

    style : 그림자, hover 색상 등theme 추가

--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -1,22 +1,26 @@
 export const light = {
-  background: '#ffffff',
+  background: "#fff",
   // sectionBackground: '#ECECEC',
-  sectionBackground: '#fff',
+  sectionBackground: "#fff",
+  hoverBackground: "#F0F0F0",
   text: '#000000',
   button: {
     toggleThemeButtonColor: '#fff',
     toggleThemeButtonHoverColor: '#333',
   },
+  shadow: `0px 0px 30px rgba(0, 0, 0, 0.25)`
 };
 
 export const dark = {
   background: '#181818',
   sectionBackground: '#181818',
+  hoverBackground: "#2D2D2D",
   text: '#ffffff',
   button: {
     toggleThemeButtonColor: '#181818',
     toggleThemeButtonHoverColor: '#ECECEC',
   },
+  shadow: `0px 0px 30px rgba(255, 255, 255, 0.25)`
 };
 
 export type Theme = typeof light;
